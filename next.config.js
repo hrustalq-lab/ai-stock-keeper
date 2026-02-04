0473f1ad9b2b2f5db854b02f4cf22721d1bfb2db
El'dar Gasanov <n1k3f1t@gmail.com>
2026-02-04 09:38:41 +0300

    Implement Epic Documents (ext.config.js)

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-  // Standalone output для Docker
+  // Standalone output для Docker / 
   output: "standalone",
 };
 
